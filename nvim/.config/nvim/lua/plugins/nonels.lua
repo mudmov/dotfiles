291c946ba6f1af6dd448ed712f7ec2d5564f76fc
mudmov <ma.abdulkerimov@gmail.com>
2025-09-22 00:30:05 +0100

    feat: added conform as formatter

--- nvim/.config/nvim/lua/plugins/nonels.lua
+++ nvim/.config/nvim/lua/plugins/nonels.lua
@@ -1,15 +1,34 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-      }
-    })
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "ruff_format" },
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          html = { "prettier" },
+          css = { "prettier" },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
 
-    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-  end
+      vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+        require("conform").format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
 }
-
