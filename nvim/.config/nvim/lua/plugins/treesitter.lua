d7176b05f19c77fb5f74f67df6e4bbdbaa0d1521
mudmov <ma.abdulkerimov@gmail.com>
2025-12-20 19:42:51 +0000

    chore: fix treesitter version to avoid breaking changes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,67 +1,52 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "master",
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   config = function()
-    local configs = require("nvim-treesitter.configs")
+    -- 1. Force the require to ensure module is loaded
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+      return
+    end
+
+    -- 2. Setup
     configs.setup({
       ensure_installed = {
-        "python",
-        "lua",
-        "javascript",
-        "html",
-        "bash",
-        "dockerfile",
-        "json",
-        "markdown",
-        "nginx",
-        "sql",
-        "yaml",
-        "css",
-        "typescript",
-        "tsx",
-        "vim",
-        "vimdoc",
-        "regex",
-        "query"
+        "python", "lua", "javascript", "html", "bash", "dockerfile",
+        "json", "markdown", "nginx", "sql", "yaml", "css",
+        "typescript", "tsx", "vim", "vimdoc", "regex", "query"
       },
+      sync_install = false,
+      auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
       indent = { enable = true },
+
+      -- Textobjects configuration inside the main setup
       textobjects = {
         enable = true,
         select = {
           enable = true,
           lookahead = true,
           keymaps = {
-            -- Functions
             ["af"] = "@function.outer",
             ["if"] = "@function.inner",
-            -- Classes
             ["ac"] = "@class.outer",
             ["ic"] = "@class.inner",
-            -- Parameters/arguments
             ["aa"] = "@parameter.outer",
             ["ia"] = "@parameter.inner",
-            -- Conditionals (if/else)
             ["ai"] = "@conditional.outer",
             ["ii"] = "@conditional.inner",
-            -- Loops
             ["al"] = "@loop.outer",
             ["il"] = "@loop.inner",
-            -- Comments
-            ["a/"] = "@comment.outer",
-            ["i/"] = "@comment.inner",
-            -- Blocks
             ["ab"] = "@block.outer",
             ["ib"] = "@block.inner",
-            -- Assignments
-            ["a="] = "@assignment.outer",
-            ["i="] = "@assignment.inner",
-            -- Calls
-            ["aF"] = "@call.outer",
-            ["iF"] = "@call.inner",
           },
         },
         move = {
@@ -70,32 +55,21 @@ return {
           goto_next_start = {
             ["]m"] = "@function.outer",
             ["]]"] = "@class.outer",
-            ["]o"] = "@loop.*",
-            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
           },
           goto_next_end = {
             ["]M"] = "@function.outer",
             ["]["] = "@class.outer",
-            ["]O"] = "@loop.*",
           },
           goto_previous_start = {
             ["[m"] = "@function.outer",
             ["[["] = "@class.outer",
-            ["[o"] = "@loop.*",
-            ["[s"] = { query = "@scope", query_group = "locals", desc = "Previous scope" },
-            ["[z"] = { query = "@fold", query_group = "folds", desc = "Previous fold" },
           },
           goto_previous_end = {
             ["[M"] = "@function.outer",
             ["[]"] = "@class.outer",
-            ["[O"] = "@loop.*",
           },
         },
       },
     })
   end,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
 }
