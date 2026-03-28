dd6fcdd41896b92211d032f372730edaef646fda
axa <axa@axa-mbx.local>
2026-03-28 14:12:31 +0000

    ralph: complete story 2 - ZSH package dependency manifest

---
+++ zsh/deps.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# ZSH package dependencies
+
+DEPS=(zsh fzf)
+
+setup() {
+  local ZSH_DIR="${HOME}/.oh-my-zsh"
+  local ZSH_CUSTOM="${ZSH_DIR}/custom"
+
+  # Install oh-my-zsh if not present
+  if [[ ! -d "$ZSH_DIR" ]]; then
+    log_info "Installing oh-my-zsh..."
+    RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  else
+    log_info "oh-my-zsh already installed"
+  fi
+
+  # Clone zsh-autosuggestions
+  local auto_dir="${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+  if [[ ! -d "$auto_dir" ]]; then
+    log_info "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$auto_dir"
+  else
+    log_info "zsh-autosuggestions already installed"
+  fi
+
+  # Clone zsh-syntax-highlighting
+  local syntax_dir="${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+  if [[ ! -d "$syntax_dir" ]]; then
+    log_info "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$syntax_dir"
+  else
+    log_info "zsh-syntax-highlighting already installed"
+  fi
+
+  # Install nvm
+  if [[ ! -d "${HOME}/.nvm" ]]; then
+    log_info "Installing nvm..."
+    PROFILE=/dev/null bash -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh)"
+  else
+    log_info "nvm already installed"
+  fi
+}
